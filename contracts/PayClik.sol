f6c3a9d1bf5f80337cd4cf00bdc4b13d17da65a7
CyberGA <etomu.joshua@gmail.com>
2023-02-08 04:48:39 +0100

    contract update

--- contracts/PayClik.sol
+++ contracts/PayClik.sol
@@ -5,15 +5,15 @@ contract PayClik {
     constructor() {}
     
     function getBalance() public view returns (uint256) {
-        return address(this).balance;
+        return address(msg.sender).balance;
     }
 
     function sendEther(address payable _to, uint256 _value) external payable {
-        require(address(this).balance >= _value, "Insufficient balance");
+        require(address(msg.sender).balance >= _value, "Insufficient balance");
+        require(_value > 0, "Amount must be greater than zero");
         require(_to != address(0), "Invalid recipient address");
-
+    
         (bool sent,) = _to.call{value: _value}("");
-
         require(sent, "Failed to send ether");
 
     }
